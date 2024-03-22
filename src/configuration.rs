01a2cdf1b465b8ae8ad174f2349cd00d9baa5aa7
throttle2k <lomax79@gmail.com>
2024-03-22 23:29:22 +0100

    Added configuration handling with yaml file

---
+++ src/configuration.rs
@@ -0,0 +1,23 @@
+use config::{File, FileFormat};
+
+#[derive(serde::Deserialize)]
+pub struct Settings {
+    pub database: DatabaseSettings,
+    pub application_port: u16,
+}
+
+#[derive(serde::Deserialize)]
+pub struct DatabaseSettings {
+    pub username: String,
+    pub password: String,
+    pub port: u16,
+    pub host: String,
+    pub database_name: String,
+}
+
+pub fn get_configuration() -> Result<Settings, config::ConfigError> {
+    config::Config::builder()
+        .add_source(File::new("configuration", FileFormat::Yaml))
+        .build()?
+        .try_deserialize()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    run(tokio::net::TcpListener::bind("127.0.0.1:3000").await?).await
+    let configuration = get_configuration().expect("A configuration file should be present");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    run(tokio::net::TcpListener::bind(address).await?).await
 }
