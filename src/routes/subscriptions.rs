b0abeefe77f381eb05fb5572e5f71da7557afa05
throttle2k <lomax79@gmail.com>
2024-03-25 21:41:47 +0100

    Added domain structs to validate name and email inputs. Added tests

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,4 +1,7 @@
-use crate::startup::AppState;
+use crate::{
+    domain::{NewSubscriber, SubscriberEmail, SubscriberName},
+    startup::AppState,
+};
 use axum::{
     extract::{Form, State},
     http::StatusCode,
@@ -27,22 +30,38 @@ pub async fn subscribe(
     State(state): State<AppState>,
     Form(form): Form<FormData>,
 ) -> Result<(), StatusCode> {
-    match insert_subscriber(&state.db_pool, &form).await {
+    let name = match SubscriberName::parse(form.name) {
+        Ok(name) => name,
+        Err(_) => return Err(StatusCode::BAD_REQUEST),
+    };
+    let email = match SubscriberEmail::parse(form.email) {
+        Ok(email) => email,
+        Err(_) => return Err(StatusCode::BAD_REQUEST),
+    };
+
+    let new_subscriber = NewSubscriber { email, name };
+    match insert_subscriber(&state.db_pool, &new_subscriber).await {
         Ok(_) => Ok(()),
         Err(_) => Err(StatusCode::INTERNAL_SERVER_ERROR),
     }
 }
 
-#[tracing::instrument(name = "Saving new subscriber in the database", skip(form, db_pool))]
-pub async fn insert_subscriber(db_pool: &PgPool, form: &FormData) -> Result<(), sqlx::Error> {
+#[tracing::instrument(
+    name = "Saving new subscriber in the database",
+    skip(new_subscriber, db_pool)
+)]
+pub async fn insert_subscriber(
+    db_pool: &PgPool,
+    new_subscriber: &NewSubscriber,
+) -> Result<(), sqlx::Error> {
     sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
         VALUES ($1, $2, $3, $4)
         "#,
         Uuid::new_v4(),
-        form.email,
-        form.name,
+        new_subscriber.email.as_ref(),
+        new_subscriber.name.as_ref(),
         Utc::now()
     )
     .execute(db_pool)
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -98,6 +98,34 @@ async fn subscribe_returns_200_for_valid_form_data() {
 }
 
 #[tokio::test]
+async fn subscribe_returns_400_when_fields_are_present_but_empty() {
+    let app = spawn_app().await.expect("Server should be up");
+    let client = reqwest::Client::new();
+
+    let test_cases = vec![
+        ("name=&email=ursula_le_guin%40gmail.com", "empty name"),
+        ("name=Ursula&email=", "empty email"),
+        ("name=Ursula&email=definitely-not-an-email", "invalid email"),
+    ];
+    for (body, description) in test_cases {
+        let response = client
+            .post(format!("{}/subscriptions", &app.address))
+            .header("Content-type", "application/x-www-form-urlencoded")
+            .body(body)
+            .send()
+            .await
+            .expect("Request should be executed");
+
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API did not return a 400 OK when the payload was {}",
+            description
+        );
+    }
+}
+
+#[tokio::test]
 async fn subscribe_returns_422_when_data_is_missing() {
     let app = spawn_app().await.expect("Server should be up");
     let client = reqwest::Client::new();
